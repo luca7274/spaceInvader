bdd4a01600099621e4c0f415453ccfb6f5b1c2d4
luca7274 <pg15uyp@eduvaud.ch>
2022-10-10 14:26:42 +0200

    ajout des test

--- spaceInvader/spaceIvaderTestUnit/UnitTest1.cs
+++ spaceInvader/spaceIvaderTestUnit/UnitTest1.cs
@@ -6,6 +6,63 @@ namespace spaceIvaderTestUnit
         [TestMethod]
         public void TestMethod1()
         {
+            int numberOne = 1;
+            int numberTwo = 2;
+            int result = numberOne + numberTwo;
+
+            Assert.AreEqual(3, result);
+        }
+    }
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int numberOne = 3;
+            int numberTwo = 4;
+            int result = numberOne + numberTwo;
+
+            Assert.AreEqual(7, result);
+        }
+    }
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int numberOne = 1;
+            int numberTwo = 10;
+            int result = numberOne + numberTwo;
+
+            Assert.AreEqual(11, result);
+        }
+    }
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int numberOne = 6;
+            int numberTwo = 2;
+            int result = numberOne + numberTwo;
+
+            Assert.AreEqual(8, result);
+        }
+    }
+    [TestClass]
+    public class UnitTest5
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int numberOne = 2;
+            int numberTwo = 2;
+            int result = numberOne + numberTwo;
+
+            Assert.AreEqual(4, result);
         }
     }
 }
