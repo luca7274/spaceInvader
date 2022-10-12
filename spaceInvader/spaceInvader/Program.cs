5fac7866edc145ba8679833efc784d32ab627588
luca7274 <pg15uyp@eduvaud.ch>
2022-10-12 16:28:57 +0200

    ajout des titre

--- spaceInvader/spaceInvader/Program.cs
+++ spaceInvader/spaceInvader/Program.cs
@@ -10,7 +10,26 @@ namespace spaceInvader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            Console.SetWindowSize(113, 51);
+            Console.Write(Menu.TITLE);
+            Console.Write(Menu.PLAY);
+            Console.Write(Menu.OPTION);
+            Console.Write(Menu.HIGHSCORE);
+            Console.Write(Menu.ABOUT);
+            Console.Write(Menu.LEAVE);
+
+            int positionX = 30;
+            int positionY = 15;
+            Console.SetCursorPosition(positionX,positionY);
+            while (true)
+            {
+                ConsoleKeyInfo keys = Console.ReadKey();
+                if(keys.Key == ConsoleKey.DownArrow)
+                {
+                    if(positionY > 14)
+                    Console.SetCursorPosition(1, 1);
+                }
+            }
             Console.ReadLine();
         }
     }
